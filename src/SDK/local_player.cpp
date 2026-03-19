3fc3264b201e31d43f84563c9e309869ba4c2edf
Orange <orange-cpp@yandex.ru>
2026-03-19 17:41:38 +0300

    update

--- src/SDK/local_player.cpp
+++ src/SDK/local_player.cpp
@@ -5,24 +5,32 @@
 #include "local_player.hpp"
 #include <omath/utility/pe_pattern_scan.hpp>
 #include <windows.h>
-#include <xorstr.hpp>
-namespace sdk
-{
-    LocalPlayer* LocalPlayer::get()
-    {
-        static auto ppLocalPlayer = *reinterpret_cast<LocalPlayer***>(
-                omath::PePatternScanner::scan_for_pattern_in_loaded_module(GetModuleHandleA(xorstr_("client.dll")),
-                                                                           xorstr_("B8 ? ? ? ? 39 10 74 10"))
-                        .value()
+
+#include "VMProtectSDK.h"
+
+namespace sdk {
+    LocalPlayer *LocalPlayer::get() {
+        static auto var = []() {
+            const auto module_name = VMProtectDecryptStringA("client.dll");
+            const auto pe_pattern = VMProtectDecryptStringA("B8 ? ? ? ? 39 10 74 10");
+            auto ppplayer = *reinterpret_cast<LocalPlayer ***>(
+                omath::PePatternScanner::scan_for_pattern_in_loaded_module(GetModuleHandleA(module_name), pe_pattern)
+                .value()
                 + 1);
-        return *ppLocalPlayer;
+
+            VMProtectFreeString(module_name);
+            VMProtectFreeString(pe_pattern);
+            return ppplayer;
+        }();
+
+        return *var;
     }
-    std::uint32_t LocalPlayer::get_flags() const
-    {
+
+    std::uint32_t LocalPlayer::get_flags() const {
         return get_by_offset<std::uint32_t>(0xF0);
     }
-    std::uint32_t LocalPlayer::is_on_ground() const
-    {
+
+    std::uint32_t LocalPlayer::is_on_ground() const {
         constexpr std::uint32_t on_ground_flag = 1 << 0;
 
         return get_flags() & on_ground_flag;
--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include <Windows.h>
 #include <omath/utility/pe_pattern_scan.hpp>
 #include <thread>
-#include <xorstr.hpp>
-
+#include <VMProtectSDK.h>
 #undef max
 LPVOID o_create_move = nullptr;
 
 // ReSharper disable once CppDFAConstantFunctionResult
-int __stdcall h_create_move(float, sdk::UserCmd* user_cmd)
+int __stdcall h_create_move(const float fl, sdk::UserCmd* user_cmd)
 {
     const auto local_player = sdk::LocalPlayer::get();
 
+    const auto result = reinterpret_cast<decltype(h_create_move)*>(o_create_move)(fl, user_cmd);
     if (local_player == nullptr)
-        return false;
+        return result;
 
     if (!local_player->is_on_ground())
         user_cmd->m_iButtons &= ~sdk::UserCmd::IN_JUMP;
@@ -28,13 +28,18 @@ int __stdcall h_create_move(float, sdk::UserCmd* user_cmd)
 
 void WINAPI hack_thread(const HMODULE module_handle)
 {
-    while (!GetModuleHandleA(xorstr_("client.dll")))
+    const auto module_name = VMProtectDecryptStringA("client.dll");
+    while (!GetModuleHandleA(module_name))
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
+    const auto pattern = VMProtectDecryptStringA("55 8B EC 6A FF E8 ?? ?? ?? ?? 83 C4 04 85 C0 75 06 B0 01");
     const auto create_move = omath::PePatternScanner::scan_for_pattern_in_loaded_module(
-                                     GetModuleHandleA(xorstr_("client.dll")),
-                                     xorstr_("55 8B EC 6A FF E8 ?? ?? ?? ?? 83 C4 04 85 C0 75 06 B0 01"))
+                                     GetModuleHandleA(module_name),
+                                     pattern)
                                      .value();
+
+    VMProtectFreeString(module_name);
+    VMProtectFreeString(pattern);
     MH_Initialize();
 
     MH_CreateHook(reinterpret_cast<LPVOID>(create_move), h_create_move, &o_create_move);
